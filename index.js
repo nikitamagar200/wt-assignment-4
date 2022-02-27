c15e8feae412b7effd9cab84eb12ae19b395658e
Nikita <nikitamagar200@gmail.com>
2022-02-27 11:12:35 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,47 @@
+function validation(){
+    var name = document.getElementById('name').value;
+    var email= document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var password= document.getElementById('password').value;
+    var cpassword = document.getElementById('cpassword').value;
+    var error_message = document.getElementById('error_message');
+    var text;
+    error_message.style.padding ="10px";
+
+  
+if(name.length<5){
+    text = "Please Enter Valid Name";
+    error_message.innerHTML = text;
+    return false;
+}
+
+if(email.indexOf('@')==-1 ||email.length<6){
+    text = "Please Enter Valid Email id";
+    error_message.innerHTML = text;
+    return false;
+}
+
+if(isNaN(phone) || phone.length!=10){
+    text = "Please Enter Valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+}
+
+if(password.length<5){
+    text = "Please Enter valid Password";
+    error_message.innerHTML = text;
+    return false;
+}
+if(cpassword.length<5){
+    text = "Please check the password";
+    error_message.innerHTML = text;
+    return false;
+}
+if(cpassword!=password){
+    text = "Please Enter correct password";
+    error_message.innerHTML = text;
+    return false;
+}
+swal("Good job!", "You clicked the button!", "success");
+return true;
+}
